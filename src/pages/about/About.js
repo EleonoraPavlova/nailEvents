bf9f75d5d28c97b6b60cdad5af5217df03f44517
eleonora <nebrudko18@gmail.com>
2021-11-01 21:33:22 +0200

    basic structure

---
+++ src/pages/about/About.js
@@ -0,0 +1,11 @@
+// import Banner from "../../pages/about/Banner";
+// import Privilege from "./Privilege";
+// import OurValues from "./OurValues";
+
+// export default function About() {
+//   return <div>
+//     <Banner />
+//     <Privilege />
+//     <OurValues />
+//   </div>
+// }
---
+++ src/pages/home/Home.js
@@ -0,0 +1,20 @@
+
+// import Banner from "../../components/home/Banner";
+// import About from "../../components/home/About";
+// import Advantage from "../../components/home/Advantage";
+// import Product from "../../components/home/Product";
+// import LastWorks from "../../components/home/LastWorks";
+// import Team from "../../components/home/Team";
+// import Subscribe from "../../components/home/Subscribe";
+
+// export default function Home() {
+//   return <div>
+//     <Banner />
+//     <About />
+//     <Advantage />
+//     <Product />
+//     <LastWorks />
+//     <Team />
+//     <Subscribe />
+//   </div>
+// }
