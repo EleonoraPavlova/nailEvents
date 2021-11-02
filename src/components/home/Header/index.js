4a48869d93b1ee21d3a9b79cdccbb95edbbc9ae3
andriibaranov <andrii@mmnedia.group>
2021-11-03 00:32:49 +0200

    add localisation

--- src/components/home/Header/index.js
+++ src/components/home/Header/index.js
@@ -1,6 +1,9 @@
 import './index.scss';
+import { useTranslation } from 'react-i18next';
 
-export default function header() {
+export default function Header() {
+    const { t, i18n } = useTranslation();
+    const currentLanguage = i18n.language;
     return <header className="topbar">
         <div className="topbar__bg">
             <div className="topbar__wrapper-nav">
@@ -10,20 +13,20 @@ export default function header() {
                             <img src={process.env.PUBLIC_URL + '/icon/logo.png'} alt="logo" />
                         </li>
                         <li className="text-middle topbar__box-item uppercase">
-                            <a href="#План-заходу">План заходу </a></li>
+                            <a href="#План-заходу">{t('NAV_MENU_PLAN')}</a></li>
                         <li className="text-middle topbar__box-item uppercase">
-                            <a href="#Реєстрація/квитки">Реєстрація/квитки</a></li>
+                            <a href="#Реєстрація/квитки">{t('NAV_MENU_REGISTER')}</a></li>
                         <li className="text-middle topbar__box-item uppercase">
-                            <a href="#Учасники">Учасники</a></li>
+                            <a href="#Учасники">{t('NAV_MENU_PARTICIPANTS')}</a></li>
                         <li className="text-middle topbar__box-item uppercase">
-                            <a href="#Спікери">Спікери</a></li>
+                            <a href="#Спікери">{t('NAV_MENU_SPEAKERS')}</a></li>
                         <li className="text-middle topbar__box-item uppercase">
-                            <a href="#Новорічна дискотека">Новорічна дискотека</a></li>
+                            <a href="#Новорічна дискотека">{t('NAV_MENU_DISCO')}</a></li>
                     </ul >
                 </div>
                 <div className="topbar__social flex">
                     <div className="topbar__social-wrap">
-                        <a href="https://www.instagram.com/pavl0vaelen/?hl=ru" target="_blank" rel="noopener" className="an">
+                        <a href="https://www.instagram.com/pavl0vaelen/?hl=ru" target="_blank" rel="noreferrer" className="an">
                             <img src={process.env.PUBLIC_URL + '/icon/insta.svg'} alt="svg" target="_blank" /></a>
                     </div>
                     <div className="topbar__social-wrap">
@@ -31,16 +34,19 @@ export default function header() {
                             <img src={process.env.PUBLIC_URL + '/icon/call.svg'} alt="call" target="_blank" /></a>
                     </div>
                     <div className="topbar__social-wrap">
-                        <a href="mailto: ntb.congress@gmail.com" target="_blank" rel="noopener" className="an">
+                        <a href="mailto: ntb.congress@gmail.com" target="_blank" rel="noreferrer" className="an">
                             <img src={process.env.PUBLIC_URL + '/icon/mail.svg'} alt="svg" target="_blank" className="topbar__social-img" /></a>
                     </div>
                     <div className="topbar__social-wrap">
-                        <a href="https://www.facebook.com/" target="_blank" rel="noopener" className="an">
+                        <a href="https://www.facebook.com/" target="_blank" rel="noreferrer" className="an">
                             <img src={process.env.PUBLIC_URL + '/icon/facebook.svg'} alt="facebook" target="_blank" /></a>
                     </div>
                     <div className="topbar__social-wrap topbar__social-wrap_youtube">
-                        <a href="https://www.youtube.com/" target="_blank" rel="noopener" className="an">
+                        <a href="https://www.youtube.com/" target="_blank" rel="noreferrer" className="an">
                             <img src={process.env.PUBLIC_URL + '/icon/youtube.png'} alt="youtube" target="_blank" /></a>
+                    </div>
+                    <div>
+                        <button onClick={() => i18n.changeLanguage(currentLanguage === 'ua' ? 'en' : 'ua')}>{ currentLanguage === 'ua' ? 'en' : 'ua' }</button>
                     </div>
                 </div>
             </div>
@@ -49,10 +55,10 @@ export default function header() {
                     <h1 className="text-title-big uppercase topbar__main-title">Nano Tech Beauty</h1>
                 </div>
                 <div>
-                    <h4 className="text-subtitle white-text">Максимум можливостей - безліч інновацій – море задоволень</h4>
+                    <h4 className="text-subtitle white-text">{t('BANNER_SUBTITLE')}</h4>
                 </div>
                 <div>
-                    <button type="button" className="topbar__button">Реєстрація</button>
+                    <button type="button" className="topbar__button">{t('REGISTRATION')}</button>
                 </div>
             </div>
         </div>
