5f78e293062662888ceee0feb5341ec680a26be6
eleonora <nebrudko18@gmail.com>
2021-11-11 10:14:08 +0200

    delete router-dom/ fix

--- src/App.js
+++ src/App.js
@@ -4,17 +4,11 @@ import { initReactI18next } from "react-i18next";
 import "./App.scss"
 
 import Home from "./pages/home/Home";
-import Tickets from "./pages/tickets/Tickets";
 import resources from './resources';
 
 import Nav from './components/common/Nav';
 import Footer from './components/common/Footer';
 
-import {
-  HashRouter as Router,
-  Switch,
-  Route,
-} from "react-router-dom";
 
 i18n
   .use(initReactI18next)
@@ -29,19 +23,12 @@ i18n
 
 export default function BasicExample() {
   return (
-    <Router>
-      <div>
-        <Nav />
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/tickets">
-            <Tickets />
-          </Route>
-        </Switch>
-        <Footer />
-      </div>
-    </Router>
+
+    <div>
+      <Nav />
+      <Home />
+      <Footer />
+    </div>
+
   )
 }
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,12 +1,12 @@
 import Header from "../../components/home/Header";
 import Organizer from "../../components/home/Organizer";
-import About from "../../components/home/About";  // про событие
+import About from "../../components/home/About";
 // import Speakers from "../../components/home/Speakers"; // спикеры
-import Statistics from "../../components/home/Statistics"; //статистика
+import Statistics from "../../components/home/Statistics";
 import YouWant from "../../components/home/YouWant";
 // import OurPartners from "../../components/home/OurPartners";  // наши партнеры??
 // import Subscribe from "../../components/home/Subscribe";  // подписка инста- розыгрыш
-import Safety from "../../components/home/Safety";     // меры безопасности
+import Safety from "../../components/home/Safety";
 import Map from "../../components/home/Map";
 // import Program from "../../components/home/Program";
 import LearnMore from "../../components/home/LearnMore";
