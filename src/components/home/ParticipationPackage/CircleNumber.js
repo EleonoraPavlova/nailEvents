f6c7c2b3ed975581002fefec69945687c7eeb41e
eleonora <nebrudko18@gmail.com>
2021-11-23 21:23:02 +0200

    change retailers

--- src/components/home/ParticipationPackage/CircleNumber.js
+++ src/components/home/ParticipationPackage/CircleNumber.js
@@ -1,9 +1,9 @@
 import './CircleNumber.scss';
 
 export default function CircleNumber(props) {
-  return <div className="circle-number__outer">
-    <div className="circle-number__inner">
+  return <div className={"circle-number__outer " + props.additionalClasses}>
+    <div className={"circle-number__inner " + props.additionalClasses}>
       {props.number}
     </div>
-  </div>
+  </ div>
 }
